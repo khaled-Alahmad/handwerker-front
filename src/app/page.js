ec15f4ed9c9ee75f29f99aa7821c9b805f1855b1
khaled alahmad <eng.khaledalahmad@gmail.com>
2024-06-25 23:04:18 +0300

    FAQ added

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import Services from "@/pages/Services/Services";
 import WhyUs from "@/pages/WhyUs/WhyUs";
 import Testimonials from "@/pages/Testimonials/Testimonials";
+import FAQ from "@/pages/FAQ/FAQ";
 
 export default function Home() {
   return (
@@ -32,6 +33,9 @@ export default function Home() {
       <WhyUs />
       <section className={styles.testimonials}>
         <Testimonials />
+      </section>
+      <section className={styles.faq}>
+        <FAQ />
       </section>
     </div>
   );
---
+++ src/pages/FAQ/FAQ.js
@@ -0,0 +1,78 @@
+"use client";
+import { useState } from "react";
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import arrowBottom from "../../../public/icons/arrow-bottom.svg";
+import arrowUp from "../../../public/icons/arrow-up.svg";
+
+const FAQ = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleFAQ = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const faqs = [
+    {
+      question: "Wie kann ich einen Termin vereinbaren?",
+      answer:
+        "Sie können einen Termin telefonisch oder online über unser Kontaktformular vereinbaren.",
+    },
+    {
+      question: "Welche Dienstleistungen bieten Sie an?",
+      answer:
+        "Wir bieten eine breite Palette von elektrischen Dienstleistungen, einschließlich Installation von Einbaugeräten, Beleuchtung, Gewerbereinigung, Laminatverlegung und Hausmeisterservice.",
+    },
+    {
+      question: "Sind Ihre Elektriker qualifiziert?",
+      answer:
+        "Ja, unsere Elektriker sind zertifiziert und haben jahrelange Erfahrung in der Branche.",
+    },
+  ];
+
+  return (
+    <section className={styles.faqSection}>
+      <h2>Warum wir?</h2>
+      {faqs.map((faq, index) => (
+        <div key={index} className={`${styles.faqItem} `}>
+          <button
+            className={`${styles.faqQuestion} ${
+              activeIndex === index ? styles.faqAnswerColor : ""
+            }`}
+            onClick={() => toggleFAQ(index)}
+          >
+            <span className={styles.iconFac}>
+              {activeIndex === index ? (
+                <Image
+                  src={arrowUp}
+                  alt=""
+                  width={52}
+                  height={52}
+                  style={{ marginRight: "0.5rem" }}
+                />
+              ) : (
+                <Image
+                  src={arrowBottom}
+                  alt=""
+                  width={52}
+                  height={52}
+                  style={{ marginRight: "0.5rem" }}
+                />
+              )}
+            </span>
+            {faq.question}
+          </button>
+          <div
+            className={`${styles.faqAnswer} ${
+              activeIndex === index ? styles.active : ""
+            }`}
+          >
+            <p>{faq.answer}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default FAQ;
